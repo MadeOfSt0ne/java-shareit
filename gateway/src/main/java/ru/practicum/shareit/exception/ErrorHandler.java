a7132c2860af9355a63fe115001b6a59d3ee8977
Artur M <artyrillo@yandex.ru>
2022-08-25 19:33:26 +0300

    checkstyle fix

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
-import java.nio.file.AccessDeniedException;
-import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -23,7 +21,7 @@ public class ErrorHandler {
     public ErrorResponse handleThrowable(final Throwable e) {
         return new ErrorResponse("Произошла непредвиденная ошибка!");
     }
-    
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleConstraintViolationException(final ConstraintViolationException e) {
